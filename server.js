b797fa78ff5653183ed01460a62cdeefb7986213
Kevin Smeraglio <ksmera01@gmail.com>
2020-05-06 16:53:59 -0400

    adding initial files and framework

---
+++ server.js
@@ -0,0 +1,17 @@
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Router
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Listener
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
