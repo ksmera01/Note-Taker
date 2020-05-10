e463cd558cf591955aa530437641f4d186ca7ff5
Kevin Smeraglio <ksmera01@gmail.com>
2020-05-10 15:48:42 -0400

    updated apiroute, got initial get working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,23 +4,24 @@ const { v4: uuidv4 } = require('uuid');
 
 module.exports = function (app) {
 
+    // let noteLibrary = [];
+
     app.get("/api/notes", function (req, res) {
-        let noteLibrary = JSON.parse(fs.readFile("/db/db.json", "utf8"));
-        console.log(noteLibrary);
-        res.json(noteLibrary);
+        return res.json(JSON.parse(fs.readFileSync("./db/db.json", "utf8")));
+        // console.log(noteLibrary);
+        // res.json(noteLibrary);
     })
 
     app.post("/api/notes", function (req, res) {
         let addNote = req.body;
         console.log(addNote);
-        JSON.parse(fs.writeFile("/db/db.json", "utf8"));
-        noteLibrary.push(addNote);
+        addNote.id = uuidv4();
+        fs.writeFile("./db/db.json", "utf8");
+        JSON.parse(noteLibrary.push(addNote));
     })
 
-    let newNoteID = uuidv4();
-    console.log(newNoteID);
 
-    app.delete("/api/notes/:id", function (req, res) {
+    // app.delete("/api/notes/:id", function (req, res) {
 
-    })
+    // })
 }
